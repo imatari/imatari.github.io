3e70af20f10ad62e542d7aa76a0b63e55cc0cf22
matari <d.upsettah@gmail.com>
2016-07-30 12:53:41 -0700

    simple mad lib : )
    a page showing my first efforts into jabba - blipp : )

---
+++ mad lib/first_program.js
@@ -0,0 +1,30 @@
+var verb = ["run","play","jump","read","laugh","breathe","fly"];
+var noun = ["dog","cat","bird","lion","wolf","house","tree"];
+var adj = ["kind","cold","smart","beautiful","glowing","fragrant","fantastic"];
+
+mkstr();
+
+function mkstr(){
+	var v1 = verb[mkrnd()];
+	var n1 = noun[mkrnd()];
+	var n2 = noun[mkrnd()];
+	var a1 = adj[mkrnd()];
+	var a2 = adj[mkrnd()];
+
+	while (n1 == n2){
+		n2 = noun[mkrnd()];
+	}
+
+	while (a1 == a2){
+		a2 = adj[mkrnd()];
+	}
+
+	var outputstr = "I like to " + v1 + ". My favorite " + n1 + " is " + a1 + " " + n2 + ". That's what makes me " + a2 + ".";
+	console.log(outputstr);
+	alert(outputstr);
+}
+
+function mkrnd(){
+	var rndNum = Math.floor((Math.random())*7);
+	return rndNum;
+}
